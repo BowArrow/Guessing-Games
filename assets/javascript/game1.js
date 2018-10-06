cb52b4b39c30d1c4a26fa2d42e09a12037b5bb39
nBow <nbow51@gmail.com>
2018-10-06 09:58:30 -0700

    content update

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -30,25 +30,66 @@ var words = [
 // var randWord = words[Math.floor(Math.random() * words.length)];
 var word = words[Math.floor(Math.random() * words.length)];
 
+
 var guessArray = [];
 for (var i = 0; i < word.length; i++) {
     guessArray[i] = "_";
 };
 
+var winSound = document.getElementById("win-sound");
+var loseSound = document.getElementById("lose-sound");
 var guessedWord = guessArray.join(" ");
-var lettersRemain = word.length;
 var wordGuess = document.getElementById("word-guess");
-wordGuess.textContent = guessedWord
+wordGuess.textContent = guessedWord;
+var guesses = document.getElementById("number-guess");
+var letterGuess = 15;
+guesses.textContent = "Guesses Left: " + letterGuess.toString();
+var letterDisplay = document.getElementById("letter-guess");
+function reset(){
+    word = words[Math.floor(Math.random() * words.length)];
+    guessArray = [];
+    for (var i = 0; i < word.length; i++) {
+        guessArray[i] = "_";
+    };
+    guessedWord = guessArray.join(" ");
+    wordGuess.textContent = guessedWord;
+    letterGuess = 15;
+    guesses.textContent = "Guesses Left: " + letterGuess.toString();
+    letterDisplay.innerHTML = "";
+};
+
 
 document.onkeyup = function (event) {
-    
     var userInput = event.key.toLowerCase();
+    if(userInput){
+        letterGuess--;
+        guesses.textContent = "Guesses Left: " + letterGuess.toString();
+        var letterDiv = document.createElement("div"); 
+        letterDiv.setAttribute("class", "float-left mx-2 center-text");
+        letterDiv.textContent = userInput.toString() + " ";
+        letterDisplay.appendChild(letterDiv);
+    }
+
     for (var j = 0; j < word.length; j++) {
-        if (word.split("")[j] == userInput) {
+      if (letterGuess === 0) {
+          wordGuess.textContent = "You lose";
+          loseSound.play();
+          setTimeout(function () {
+            reset();
+        }, 2000);
+    } else if (word.split("")[j] == userInput) {
             guessArray[j] = userInput;
             wordGuess.textContent = guessArray.join(" ");
-        }
+            if (word === guessArray.join("")){
+                wordGuess.textContent = "YOU WIN!";
+                winSound.play();
+                setTimeout(function () {
+                    reset();
+                }, 2000);
+            };    
+        };
+    };
 
-    }
+    
 
 };
